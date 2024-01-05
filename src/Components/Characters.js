0100cb0ad87788fc21b7dea64c97edb96d452f8d
BriseydaMF <briseyda.flores29@gmail.com>
2024-01-04 18:49:24 -0600

    Updated

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,19 +1,7 @@
 export default function Characters(props) {
-    const { characters } = props;
-    
-
   return (
-    <div className="characters"> 
-
-        <h1>Personajes</h1>
-        <span className="back-home">Volver a la home</span>
-        <div className="container-characters">
-            {characters.map((character, index) => (
-                <div className="character-container" key={index}>
-                    <p>{character.name}</p> 
-                </div>
-            ))}
-        </div>
+    <div>
+        <h1>Desde characters JS</h1>
     </div>
-  );
+  )
 }
